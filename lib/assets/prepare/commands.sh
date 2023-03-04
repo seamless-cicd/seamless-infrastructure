e915388c772530a47389333310744dc22ce89112
Jason Wang <jasonherngwang@gmail.com>
2023-03-04 08:05:43 -0800

    chore: add informative messages to shell script

--- lib/assets/prepare/commands.sh
+++ lib/assets/prepare/commands.sh
@@ -1,17 +1,33 @@
-echo "cloning from $GH_REPO using $GH_PAT"
-rm -rf /data/app
-mkdir -p /data/app
+# If repo was previously cloned, remove it
+if [ -d "/data/app" ]; then
+    echo "Deleting existing source code folder"
+    rm -rf "/data/app"
+    mkdir -p "/data/app"
+fi
 
-# TODO: Git pull if repository already exists?
+# Clone repository from GitHub using personal access token
+echo "Cloning source code from $GH_REPO using $GH_PAT"
 
-echo "cloning"
-git clone $GH_REPO /data/app
-echo "done cloning"
+if git clone "$GH_REPO" "/data/app"; then
+    echo "Cloning succeeded"
+else
+    echo "Cloning failed; deleting source code folder"
+    rm -rf "/data/app"
+    exit 1
+fi
 
-cd /data/app
+cd "/data/app"
 
-echo "installing"
+# Install dependencies using npm
+echo "Installing dependencies"
+
 npm config set fetch-retry-mintimeout 20000
 npm config set fetch-retry-maxtimeout 120000
-npm install
-echo "done installing"
+
+if npm ci; then
+    echo "Installing dependencies succeeded"
+else
+    echo "Installing dependencies failed; deleting source code folder"
+    rm -rf "/data/app"
+    exit 1
+fi
